77f915f6f98de1e98cc7564043b0dc5269e3a11e
devpsicoamgg <psico.amgg@gmail.com>
2024-02-16 22:19:25 -0500

    Adding coordinator Model

--- backend/src/3.-DataBase/dataBaseConfig.js
+++ backend/src/3.-DataBase/dataBaseConfig.js
@@ -7,6 +7,7 @@ const Family = require("./models/Family");
 const Group = require("./models/Group");
 const TeamIntervention = require("./models/TeamIntervention");
 const UserFinal = require("./models/UserFinal");
+const Coordinator = require("./models/Coordinator");
 
 
 
@@ -24,19 +25,22 @@ const FamilyModel = Family(sequelize, DataTypes);
 const GroupModel = Group(sequelize, DataTypes);
 const TeamInterventionModel = TeamIntervention(sequelize, DataTypes);
 const UserFinalModel = UserFinal(sequelize, DataTypes);
+const CoordinatorModel = Coordinator(sequelize, DataTypes);
 
 
-ContractModel.hasOne(TeamInterventionModel, { foreignKey: 'contractId' });
-ContractModel.hasMany(UserFinalModel, { foreignKey: 'contractId' });
+ContractModel.hasMany(CoordinatorModel, { foreignKey: 'contractId' });
+CoordinatorModel.belongsTo(ContractModel, { foreignKey: 'contractId' });
 
-TeamInterventionModel.belongsTo(ContractModel, { foreignKey: 'contractId' });
+CoordinatorModel.hasMany(TeamInterventionModel, { foreignKey: 'coordinatorId' });
+TeamInterventionModel.belongsTo(CoordinatorModel, { foreignKey: 'coordinatorId' });
+
+TeamInterventionModel.hasMany(GroupModel, { foreignKey: 'teamInterventionId' });
+GroupModel.belongsTo(TeamInterventionModel, { foreignKey: 'teamInterventionId' });
+
+GroupModel.hasMany(UserFinalModel, { foreignKey: 'groupId' });
+UserFinalModel.belongsTo(GroupModel, { foreignKey: 'groupId' });
 
 FamilyModel.hasMany(UserFinalModel, { foreignKey: 'familyId' });
-
-GroupModel.belongsTo(UserFinalModel, { foreignKey: 'groupId' });
-GroupModel.hasMany(UserFinalModel, { foreignKey: 'groupId' });
-
-UserFinalModel.belongsTo(GroupModel, { foreignKey: 'groupId' });
 UserFinalModel.belongsTo(FamilyModel, { foreignKey: 'familyId' });
 
 
---
+++ backend/src/3.-DataBase/models/Coordinator.js
@@ -0,0 +1,234 @@
+const { DataTypes } = require("sequelize");
+
+const Coordinators = (sequelize) => {
+  const Coordinators = sequelize.define(
+    "Coordinators",
+    {
+      // Primary key
+      id: {
+        type: DataTypes.UUID,
+        allowNull: false,
+        primaryKey: true,
+        defaultValue: DataTypes.UUIDV4,
+      },
+
+      // Personal Information
+      firstName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+
+      secondName: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+
+      firstLastName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+
+      secondLastName: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+
+      // Document Information
+      kindDoc: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+
+      numberDoc: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+
+      // Birth Information
+      birthDepartment: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      birthMunicipality: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      nataleDate: {
+        type: DataTypes.DATEONLY,
+        allowNull: true,
+      },
+
+      // Marital and Disability Information
+      maritalStatus: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+
+      disability: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false,
+        allowNull: true,
+      },
+
+      // Gender and SISBEN Information
+      gender: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+
+      // Health System Information
+      healthSystemAffiliation: {
+        type: DataTypes.BOOLEAN,
+        allowNull: true,
+      },
+
+      regimen: {
+        type: DataTypes.ENUM("Subsidiado", "Contributivo", "Especial"),
+        allowNull: true,
+      },
+
+      kindHealthSystem: {
+        type: DataTypes.ENUM("Cotizante", "Beneficiario"),
+        allowNull: true,
+      },
+
+      // Ethnicity and Occupation
+      ethnicity: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+
+      createdInDb: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: true,
+      },
+
+      status: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: true,
+      },
+
+      entryDate: {
+        type: DataTypes.DATE,
+        allowNull: true,
+      },
+
+      reasonForLeaving: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+
+      exitDate: {
+        type: DataTypes.DATE,
+        allowNull: true,
+      },
+
+      phone: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      maxEducationalLevel: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      kindFormation: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      typeOfFormation: {
+        type: DataTypes.ENUM("Vocational", "Foreign"),
+        allowNull: true,
+      },
+      lastYearHighSchool: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+      },
+      highSchoolCompletionDate: {
+        type: DataTypes.DATEONLY,
+        allowNull: true,
+      },
+      university: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      lastLevelEducationDate: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      postgraduateTitle: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      trainingNeeds: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      experienceWorkingEarlyChildhood: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      accessToGoodsServices: {
+        type: DataTypes.ARRAY(DataTypes.STRING),
+        allowNull: true,
+      },
+      computer: {
+        type: DataTypes.BOOLEAN,
+        allowNull: true,
+      },
+      internetAccess: {
+        type: DataTypes.BOOLEAN,
+        allowNull: true,
+      },
+      contractType: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      communityMother: {
+        type: DataTypes.BOOLEAN,
+        allowNull: true,
+      },
+      healthSocialSecurity: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      eps: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      arl: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      pensionFund: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      compensationFund: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+
+      role: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+
+      imgDoc: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+    },
+    { timestamps: true }
+  );
+
+  return Coordinators;
+};
+
+module.exports = Coordinators;
