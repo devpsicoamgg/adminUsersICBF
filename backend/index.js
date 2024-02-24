edff69be367fff06e5cc2c7630c376ba5cf3e623
devpsicoamgg <psico.amgg@gmail.com>
2024-02-23 21:41:08 -0500

    RenderOne

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,11 @@
-require('dotenv').config();
 const server = require("./src/server");
-const { sequelize }
- = require("../backend/src/3.-DataBase/dataBaseConfig")
-const { PORT_SERVER } = process.env;
+const { sequelize } = require("../backend/src/3.-DataBase/dataBaseConfig");
 
+// const PORT_SERVER = 3001
+
+const PORT_SERVER = 5432;
 
 server.listen(PORT_SERVER, () => {
-  sequelize.sync({alter: true})
+  sequelize.sync({ alter: true });
   console.log(`Server is live & listening on port 🎉 ${PORT_SERVER} 🎈`);
 });
