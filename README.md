57ad2499cf0251cda07f63c08d43d78639582887
devpsicoamgg <psico.amgg@gmail.com>
2024-02-27 22:59:32 -0500

    More

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-.md
+[Enlace a ROUTES.MD](/backend/src/1.-Routes/ROUTES.MD)
--- backend/src/3.-DataBase/dataBaseConfig.js
+++ backend/src/3.-DataBase/dataBaseConfig.js
@@ -17,10 +17,10 @@ const sequelize = new Sequelize(
   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`,
   {
       dialectOptions: {
-      ssl: {
+       ssl: {
        require: true,
        rejectUnauthorized: false,
-     },  
+     },   
     }, 
     logging: false,
     native: false,
